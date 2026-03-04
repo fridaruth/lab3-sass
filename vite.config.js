54bfc12c3e88453ea9e58d271a24acdc152658cf
Frida <fridapihlstrom@gmail.com>
2026-03-04 14:36:14 +0100

    Added and styled content

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,28 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 export default defineConfig({
     build: {
         rollupOptions: {
             input: {
-                main: resolve(__dirname, "index.html"),
+                index: resolve(__dirname, "index.html"),
                 sass: resolve(__dirname, "sass.html"),
-            }
-        }
-    }
+            },
+        },
+    },
+plugins: [
+        ViteImageOptimizer({
+            jpg: {
+                quality: 80,
+            },
+            png: {
+                quality: 80,
+            },
+            webp: {
+                quality: 80,
+            },
+        }),
+    ],
 })
+
