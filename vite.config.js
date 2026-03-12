397c5852d467f5c4fd495731c2f9569ce14e1fab
Frida <fridapihlstrom@gmail.com>
2026-03-12 15:33:57 +0100

    update image optimizer configuration to exclude jsdoc

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,7 @@ export default defineConfig({
     },
 plugins: [
         ViteImageOptimizer({
+            exclude: [/\bjsdoc\b/],
             jpg: {
                 quality: 80,
             },
